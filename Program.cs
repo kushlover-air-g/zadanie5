8c20f82ae019b8f5d63799cce8d69c14ea6d91b5
kushlover-air-g <59054712+kushlover-air-g@users.noreply.github.com>
2019-12-19 20:10:19 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int delit = 1, chisl;
+            Console.WriteLine("Введите номер цифры последовательности:");
+            int k = Convert.ToInt32(Console.ReadLine());
+            if(k<=9)
+            {
+                Console.WriteLine(k + "цифра в последовательности:");
+                Console.WriteLine(k);
+            }
+            else
+            {
+                if(k%2==0)
+                {
+                    Console.WriteLine(k + "цифра в последовательности:");
+                    Console.WriteLine(k/10);
+                }
+                else
+                {
+                    for(int i = 10;k>i;i*=10)
+                    {
+                        delit = i;
+                    }
+                    for(int i = delit;k>10;i/=10)
+                    {
+                        chisl = k / i;
+                        k = k - (chisl * i);
+                    }
+                    Console.WriteLine(k + "цифра в последовательности:");
+                    Console.WriteLine(k);
+                }
+            }
+        }
+    }
+}
